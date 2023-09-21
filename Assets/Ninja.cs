d943555124a5d9bba56e8e2c04c22cf4c8422881
CeniusM <93879193+CeniusM@users.noreply.github.com>
2023-09-21 11:19:32 +0200

    Have added Feet, hands and a body

---
+++ Assets/Ninja.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public float Hp;
+public float Dmg;
+public float AtcSpeed;
+public float Speed;
+ */
+
+public class Ninja : Player
+{
+    public float Hp;
+    public Body Body;
+    public Hand LeftHand;    
+    public Hand RightHand;    
+    public Foot LeftFoot;    
+    public Foot RightFoot;
+
+
+}
